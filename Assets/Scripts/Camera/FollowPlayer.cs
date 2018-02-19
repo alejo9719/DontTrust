2d2b5b6169e293ef42abac9676aca03d08f023e1
Javier Alejandro Velasco Zapata <jvelasco65@puj.edu.co>
2018-02-19 08:23:35 -0500

    First commit

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject Player;
+	public Vector3 offset;
+	private ThirdPersonCharacter character;
+	private Vector3 newPosition;
+
+	void Start()
+	{
+		//offset = transform.position - Player.transform.position;
+		offset= new Vector3(3f, 1.2f, 0f);
+		newPosition = Player.transform.position + offset;
+	}
+
+	void LateUpdate()
+	{
+		character= Player.GetComponent<ThirdPersonCharacter>();
+
+		if (!character.m_IsGrounded) {
+			newPosition.x += 0.05f;
+		} 
+		else if (newPosition.x >= Player.transform.position.x + offset.x) {
+			newPosition.x -= 0.1f;
+		}
+
+		newPosition.y += Player.transform.position.y + offset.y - newPosition.y;
+		newPosition.z += Player.transform.position.z + offset.z - newPosition.z;
+		transform.position = newPosition;
+	}
+}
