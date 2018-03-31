a4e8bf8b09b95e822114c5c3d04fc68ee1d19d07
LAPTOP-NVF25T4I\JavierA <alejo9719@gmail.com>
2018-03-31 11:45:45 -0500

    Crouching Enemy Shooting
    - Enemy now shoots bullets which push the character back

--- Assets/CrouchedEnemy.cs
+++ Assets/CrouchedEnemy.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CrouchedEnemy : MonoBehaviour {
 
+	public GameObject prefab;
+	public float fireRate;
+	private float nextFire = 0f;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			GameObject bullet = Instantiate( prefab, new Vector3(transform.position.x-0.5f, transform.position.y+3.2f, transform.position.z-6f), 
+				Quaternion.Euler(90f, 0f, 0f) ) as GameObject; //Instantiate new "prefab" object (bullet) on the specified position and rotation
+			//cube.name = "Cube" + count++;
+			bullet.GetComponent<Rigidbody>().AddForce(-Vector3.forward*80, ForceMode.Impulse);
+			Destroy(bullet, 2f); //Destroy the bullet 2 seconds later
+
+		}
 	}
 }
