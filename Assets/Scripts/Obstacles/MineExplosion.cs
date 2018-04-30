01095f36bbd7fd78550ba000737ae9b38c618bf6
Juan David Ospina <jdospina1995@gmail.com>
2018-04-29 20:22:59 -0500

    Obstacle and Instructions
    Added the land mine obstacle, explosion particle system, plus a few set of instructions to be displayed as background objects in UI during the tutorial level

---
+++ Assets/Scripts/Obstacles/MineExplosion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DontTrust.Characters.Main;
+
+public class MineExplosion : MonoBehaviour {
+
+	[SerializeField] private sbyte m_Damage = 10;
+
+	public AudioClip Explosion; // Explosion sound
+
+	private GameObject Mine; // Land Mine
+
+	private MainCharacter m_MCharacter; // Game Character
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().Stop (); // Stop the animation so it doesn't play until the soldier collides with it
+
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			GetComponent<ParticleSystem>().Play (); // Start the animation after the collision
+			GetComponent<AudioSource>().PlayOneShot (Explosion); // Play sound for the explosion
+			Destroy (GetComponent<ParticleSystem>()); // End explosion animation
+			Mine = GameObject.Find ("Mine"); // Find Game Object Mine
+			Mine.GetComponent<MeshRenderer> ().enabled = false; //Hide Mesh after collision for the mine to disappear
+			m_MCharacter = other.gameObject.GetComponent<MainCharacter>(); //Get the MainCharacter component (class) of the player's gameObject
+			m_MCharacter.TakeDamage(m_Damage); //Decrease player's health when colliding with it
+		}
+	}
+}
