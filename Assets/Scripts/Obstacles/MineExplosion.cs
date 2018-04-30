f4391682e7ff31be85cac2dc7e813584168e47ee
Juan David Ospina <jdospina1995@gmail.com>
2018-04-30 09:42:54 -0500

    Fixed land mine issue
    Fixed the land mine to disappear and to push the character back on explosion

--- Assets/Scripts/Obstacles/MineExplosion.cs
+++ Assets/Scripts/Obstacles/MineExplosion.cs
@@ -12,6 +12,7 @@ public class MineExplosion : MonoBehaviour {
 	private GameObject Mine; // Land Mine
 
 	private MainCharacter m_MCharacter; // Game Character
+	private Rigidbody m_Rigidbody; // Game Character's rigid body
 
 	// Use this for initialization
 	void Start () {
@@ -24,11 +25,14 @@ public class MineExplosion : MonoBehaviour {
 		if (other.tag == "Player") {
 			GetComponent<ParticleSystem>().Play (); // Start the animation after the collision
 			GetComponent<AudioSource>().PlayOneShot (Explosion); // Play sound for the explosion
-			Destroy (GetComponent<ParticleSystem>()); // End explosion animation
 			Mine = GameObject.Find ("Mine"); // Find Game Object Mine
-			Mine.GetComponent<MeshRenderer> ().enabled = false; //Hide Mesh after collision for the mine to disappear
 			m_MCharacter = other.gameObject.GetComponent<MainCharacter>(); //Get the MainCharacter component (class) of the player's gameObject
 			m_MCharacter.TakeDamage(m_Damage); //Decrease player's health when colliding with it
+			Vector3 JumpForce = new Vector3(0, 12f*2, -70);
+			m_Rigidbody = other.gameObject.GetComponent<Rigidbody>(); //Get the Rigid Body of the MainCharacter component (class) of the player's gameObject
+			m_Rigidbody.AddForce (JumpForce, ForceMode.Impulse);
+			Destroy (GetComponent<ParticleSystem>()); // End explosion animation
+			Mine.GetComponent<MeshRenderer> ().enabled = false; //Hide Mesh after collision for the mine to disappear
 		}
 	}
 }
