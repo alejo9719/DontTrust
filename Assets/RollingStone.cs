c18f2d56c40c0998a98678e369a2c9bb92bf339a
LAPTOP-NVF25T4I\JavierA <alejo9719@gmail.com>
2018-05-07 11:22:14 -0500

    ROlling Stone Behaviour
    - Rolling stone obstacle triggers when character approaches.
    - Crouched enemy rig fixes.

---
+++ Assets/RollingStone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingStone : MonoBehaviour {
+
+	[SerializeField] private float m_InitialSpeed = 30;
+	private Rigidbody m_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			m_Rigidbody.useGravity = true;
+			m_Rigidbody.AddForce (Vector3.down*m_InitialSpeed, ForceMode.VelocityChange);
+		}
+	}
+}
