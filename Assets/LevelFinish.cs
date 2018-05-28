e94e1068568bdcee7a92946ff038ff69d1b4f4b5
Juan Fernando Otoya <jfotoya@javerianacali.edu.co>
2018-05-28 09:37:11 -0500

    Entrega

---
+++ Assets/LevelFinish.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1); //Loads next scene
+		}
+	}
+}
