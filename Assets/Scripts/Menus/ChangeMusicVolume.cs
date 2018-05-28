b622478914063a92aacdf461a0b022134bbaba23
Juan David Ospina <jdospina1995@gmail.com>
2018-05-27 23:47:24 -0500

    Rocks added to level 1 and music conf
    Added new obstacle in level 1 so the player couldn't go back as soon as the level starts.
    
    Added script to volume mixer in order to fix the volume settings in the main menu

---
+++ Assets/Scripts/Menus/ChangeMusicVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMusicVolume : MonoBehaviour {
+
+	public Slider Volume;
+	public AudioSource Music;
+
+	// Update is called once per frame
+	void Update () {
+		Music.volume = Volume.value;
+	}
+}
